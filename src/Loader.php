464318f727bead00868055de89fae2d0ede2c85c
Lukas Kahwe Smith <smith@pooteeweet.org>
2018-03-05 12:31:53 +0100

    refactored to using classes

--- src/Loader.php
+++ src/Loader.php
@@ -1,20 +1,8 @@
 <?php
 
-/*
- * This file is part of Twig.
- *
- * (c) 2009 Fabien Potencier
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Frctl;
 
-/**
- * Loads template from the filesystem.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- */
-class Alias_Loader implements Twig_LoaderInterface, Twig_ExistsLoaderInterface
+class Loader implements \Twig_LoaderInterface, \Twig_ExistsLoaderInterface
 {
 
     protected $aliases = array();
